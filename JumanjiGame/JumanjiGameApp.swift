52b0fac8de4fb487eb626330b48f4599013caa7b
Giorgio Giannotta <jorge_upa@msn.com>
2023-01-28 09:16:13 +0100

    Initial Commit

---
+++ JumanjiGame/JumanjiGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  JumanjiGameApp.swift
+//  JumanjiGame
+//
+//  Created by Giorgio Giannotta on 28/01/23.
+//
+
+import SwiftUI
+
+@main
+struct JumanjiGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
